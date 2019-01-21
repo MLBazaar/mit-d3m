d4149dac96c337f76f88f6a65e1912a9efe90780
Carles Sala <carles@pythiac.com>
2019-01-21 14:55:37 -0500

    Fix dependencies

--- setup.py
+++ setup.py
@@ -15,9 +15,11 @@ install_requires = [
     'networkx==2.1',
     'numpy>=1.15.2',
     'pandas>=0.23.4',
+    'psutil>=5.4.8',
     'pymongo>=3.7.2',
     'scikit-learn>=0.20.0',
     'scipy>=1.1.0',
+    'tensorflow>=1.11.0',
 ]
 
 setup_requires = [
