0ed70a0ab53667a63f037b758a4f985d1e60f02c
Carles Sala <carles@pythiac.com>
2019-11-14 13:59:24 -0500

    Bump version: 0.2.1 → 0.2.2.dev0

--- setup.py
+++ setup.py
@@ -93,6 +93,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/mit-d3m',
-    version='0.2.1',
+    version='0.2.2.dev0',
     zip_safe=False,
 )
